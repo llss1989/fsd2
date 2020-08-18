1c30f4b446ebbfe5795a99fec4738d25c8b04f43
llss1989 <argenkasymov2@gmail.com>
2020-08-18 11:50:12 +0300

    остановился на создании varsFonts

--- dist/main.js
+++ dist/main.js
@@ -163,7 +163,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! jquery */ \"../node_modules/jquery/dist/jquery.js\");\n/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _styles_general_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles/_general.scss */ \"./styles/_general.scss\");\n/* harmony import */ var _styles_general_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_general_scss__WEBPACK_IMPORTED_MODULE_1__);\n\n\nconsole.log(\"development\");\n\n//# sourceURL=webpack:///./index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! jquery */ \"../node_modules/jquery/dist/jquery.js\");\n/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _styles_general_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles/_general.scss */ \"./styles/_general.scss\");\n/* harmony import */ var _styles_general_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_general_scss__WEBPACK_IMPORTED_MODULE_1__);\n\n\n\n//# sourceURL=webpack:///./index.js?");
 
 /***/ }),
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
-import *as $ from 'jquery'
+import *as $ from 'jquery';
 import './styles/_general.scss'
-
-
-console.log(process.env.NODE_ENV)
