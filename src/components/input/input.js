81374884d38c7f8615c6277dd3295cf405b6047a
llss1989 <argenkasymov2@gmail.com>
2021-11-29 10:05:34 +0300

    Добавлен компонент registrationForm

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -6,8 +6,8 @@ const inputs = () => {
     Inputmask('99.99.9999', { placeholder: 'ДД.ММ.ГГГГ' }).mask(dateInput);
     const emailInput = document.querySelectorAll('.input__email');
     Inputmask('email', { placeholder: 'Email' }).mask(emailInput);
-    const textInput = document.querySelectorAll('.input__text');
-    Inputmask('9-a{1,3}9{1,3}').mask(textInput);
+    // const passwordInputs = document.querySelectorAll('.input__password');
+    // Inputmask('password', { placeholder: 'Пароль' }).mask(passwordInputs);
   });
 };
 inputs();
